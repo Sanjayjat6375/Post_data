d3a3b54b5fe0483d6cf65978dbff23e1e1131084
SanjayChoudharyWeb <154726029+SanjayChoudharyWeb@users.noreply.github.com>
2024-06-08 20:32:01 +0530

    wonderlust

--- cloudConfig.js
+++ cloudConfig.js
@@ -1,10 +1,10 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js', // Adjust the entry point as per your project structure
+  entry: 'app.js', // Adjust the entry point as per your project structure
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'init')
   },
   module: {
     rules: [
